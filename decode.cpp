d379301c244ef6704304fe896e6685b97e6cf3a5
Andrey <stroganov.aa@phystech.edu>
2019-03-03 18:50:05 +0300

    aaa

--- decode.cpp
+++ decode.cpp
@@ -3,103 +3,279 @@
 #define slice(val, start, stop) (((val) >> (start)) & ((1 << (stop-start+1)) - 1))
 
 //тип инструкции зависит от опкода
+
 typedef enum{
-	R_Type,
-	I_Type,
-	S_Type,
-	B_Type,
-	U_Type,
-	J_Type
-}InstrType;
+	LUI, 
+	AUIPC,
+	JAL,
+	JALR,
+	BEQ,
+	BNE,
+	BLT,
+	BGE,
+	BLTU,
+	BGEU,
+	LB,
+	LH,
+	LW,
+	LBU,
+	LHU,
+	SB,
+	SH,
+	SW,
+	ADDI,
+	SLTI,
+	SLTIU,
+	XORI,
+	ORI,
+	ANDI,
+	SLLI,
+	SRLI,
+	SRAI,
+	ADD,
+	SUB,
+	SLL,
+	SLTU,
+	XOR,
+	SRL,
+	SRA,
+	OR,
+	AND,
+	FENCE,
+	FENCEI,
+	ECALL,
+	EBREAK,
+	CSRRW,
+	CSRRS,
+	CSRRC,
+	CSRRWI,
+	CSRRSI,
+	CSRRCI
+}Instruction;
+
 
 class Decode{
 	public:
-		uint8_t opcode;
 		uint8_t rd;
-		uint8_t funct3;
 		uint8_t rs1;
 		uint8_t rs2;
-		uint8_t funct7;
 		uint32_t imm;
-		InstrType inst_type;
+		uint8_t shamt;
+		Instruction instr;
 
-		Decode():opcode(0), rd(0), funct3(0), rs1(0), rs2(0), funct7(0), imm(0), inst_type(R_Type){}
-		Decode(uint8_t opcode, uint8_t rd, uint8_t funct3, uint8_t rs1, uint8_t rs2, uint8_t funct7, uint32_t imm, InstrType inst_type);
-		uint8_t get_opcode(){return opcode;};
-		uint8_t get_rd(){return rd;};
-		uint8_t get_funct3(){return funct3;};	
-		uint8_t get_rs1(){return rs1;};	
-		uint8_t get_rs2(){return rs2;};	
-		uint8_t get_funct7(){return funct7;};	
-		uint32_t get_imm(){return imm;};	
-		InstrType get_inst_type(){return inst_type;};
+		Decode():rd(0), rs1(0), rs2(0), imm(0), instr(ADD){}
+		Decode(uint8_t rd_, uint8_t rs1_, uint8_t rs2_, uint32_t imm_, uint8_t shamt, Instruction instr_);
 	
 		
 };
 
 
-Decode::Decode(uint8_t opcode_, uint8_t rd_, uint8_t funct3_, 
-						uint8_t rs1_, uint8_t rs2_, uint8_t funct7_, uint32_t imm_, InstrType inst_type_){
-	opcode = opcode_;
+Decode::Decode(uint8_t rd_, uint8_t rs1_, uint8_t rs2_, uint32_t imm_, uint8_t shamt, Instruction instr_)
+{
 	rd = rd_;
-	funct3 = funct3_;
 	rs1 = rs1_;
 	rs2 = rs2_;
-	funct7 = funct7_;
 	imm = imm_;
-	inst_type = inst_type_;
+	shamt = shamt_;
+	instr = instr_;
 }
 
-
-
 void decode_R_type(Decode &obj, uint32_t instr){
-	obj.opcode = slice(instr, 0, 6);
 	obj.rd = slice(instr, 7, 11);
-	obj.funct3 = slice(instr, 12, 14);
 	obj.rs1 = slice(instr, 15, 19);
 	obj.rs2 = slice(instr, 20, 24);
-	obj.funct7 = slice(instr, 25, 31);
 }
 
 void decode_I_type(Decode &obj, uint32_t instr){
-	
-	obj.opcode = slice(instr, 0, 6);
 	obj.rd = slice(instr, 7, 11);
-	obj.funct3 = slice(instr, 12, 14);
 	obj.rs1 = slice(instr, 15, 19);
 	obj.imm = slice(instr, 20, 31);
 }
 
-void set_type_and_decode(Decode &obj, uint32_t instr){
-	uint8_t opcode = slice(instr, 0, 6);
-	switch(opcode){
-		case 0b0000000:{ //R_type
-			decode_R_type(obj, instr);
-			std::cout << "R_type instruction" << std::endl;
-			break;
-		}
-		case 0b0000011:{//I_type
-			decode_I_type(obj, instr);
-			std::cout << "I_type instruction" << std::endl;
-			break;
-		}
-		case 0b0100011:{//S_type
-			break;
-		}
-		case 0b1100011:{//b_type
-			break;
-		}
-
-	}
+void decode_S_type(Decode &obj, uint32_t instr){
+}
+void decode_B_type(Decode &obj, uint32_t instr){
 }
 
+void decode_U_type(Decode &obj, uint32_t instr){
+	obj.rd = slice(instr, 7, 11);
+	obj.imm = slice(instr, 12, 31);
+}
+void decode_J_type(Decode &obj, uint32_t instr){
+	obj.rd = slice(instr, 7, 11);
+	obj.imm = slice(instr, 12, 31); //check
+}
+
+
+void set_type_and_decode(Decode &obj, uint32_t instr){
+	uint8_t opcode = slice(instr, 0, 6);
+	if (opcode == 0b0110111){
+		decode_U_type(obj, instr);
+		obj.Instruction = LUI;
+	}
+	else if(opcode == 0b0010111){
+		decode_U_type(obj, instr);
+		obj.Instruction = AUIPC;
+	}
+	else if(opcode == 0b1101111){
+		decode_J_type(obj, instr);
+		obj.Instruction = JAL;
+	}
+	else if(opcode == 1100111){
+		decode_I_type(obj, instr);
+		obj.Instruction = JALR;
+	}
+	else if(opcode == 1100011){
+		decode_B_type(obj, instr);
+		uint8_t funct3 = slice(instr, 12,14);
+		switch(funct3){
+			case 0b000:{
+				obj.Instruction = BEQ;
+				break; 			}
+			case 0b001:{
+				obj.Instruction = BNE;
+				break; 			}
+			case 0b100:{
+				obj.Instruction = BLT;
+				break;  		}
+			case 0b101:{
+				obj.Instruction = BGE;
+				break;  		}
+			case 0b110:{
+				obj.Instruction = BLTU;
+				break; 			}
+			case 0b111:{
+				obj.Instruction = BGEU;
+				break; 			}
+
+		}
+	}
+	else if (opcode == 0b0000011){
+		decode_I_type(obj, instr);
+		uint8_t funct3 = slice(instr, 12,14);
+		switch(funct3){
+			case 0b000:{
+				obj.Instruction = LB;
+				break;		}
+			case 0b001:{
+				obj.Instruction = LH;
+				break;    	}
+			case 0b010:{
+				obj.Instruction = LW;
+				break;    	}
+			case 0b100:{
+				obj.Instruction = LBU;
+				break;    	}
+			case 0b101:{
+				obj.Instruction = LHU;
+				break;   	}
+		}
+	}
+	else if (opcode == 0b0100011){
+		decode_S_type(obj, instr);
+		uint8_t funct3 = slice(instr, 12,14);
+		switch(funct3){
+			case 0b000:{
+				obj.Instruction = SB;
+				break;		}
+			case 0b001:{
+				obj.Instruction = SH;
+				break;    	}
+			case 0b010:{
+				obj.Instruction = SW;
+				break;    	}
+		}
+	}
+	else if (opcode == 0b0100011){
+		decode_I_type(obj, instr);
+		uint8_t funct3 = slice(instr, 12,14);
+		switch(funct3){
+			case 0b000:{
+				obj.Instruction = ADDI;
+				break;		}
+			case 0b001:{
+				obj.Instruction = SLTI;
+				break;    	}
+			case 0b011:{
+				obj.Instruction = SLTIU;
+				break;    	}
+			case 0b100:{
+				obj.Instruction = XORI;
+				break;		}	
+			case 0b110:{
+				obj.Instruction = ORI;
+				break;		}
+			case 0b111:{
+				obj.Instruction = ANDI;
+				break;		}
+	}
+	else if (opcode == 0b0010011){
+		decode_I_type(obj, instr);
+		uint8_t funct3 = slice(instr, 12,14);
+		switch(funct3){
+			case 0b001:{
+				obj.Instruction = SLLI;
+				break;		}
+			case 0b101:{
+				uint8_t funct7 = slice(instr, 25, 31);
+				if (funct7 == 0b0000000)
+					obj.Instruction = SRLI;
+				else if (funct7 == 0b0100000)
+					obj.Instruction = SRAI;
+				break;		}
+
+		}
+	}
+	else if (opcode == 0b0110011){
+		decode_R_type(obj, instr);
+		uint8_t funct3 = slice(instr, 12,14);
+		switch(funct3){
+			case 0b000:{
+				uint8_t funct7 = slice(instr, 25, 31);
+				if (funct7 == 0b0000000)
+					obj.Instruction = ADD;
+				else if (funct7 == 0b0100000)
+					obj.Instruction = SUB;
+				break;		}
+			case 0b101:{
+				uint8_t funct7 = slice(instr, 25, 31);
+				if (funct7 == 0b0000000)
+					obj.Instruction = SRL;
+				else if (funct7 == 0b0100000)
+					obj.Instruction = SRA;
+				break;		}
+			case 0b001:{
+				obj.Instruction = SLL;
+				break;
+				}
+			case 0b010:{
+				obj.Instruction = SLT;
+				break;	
+			}
+			case 0b011:{
+				obj.Instruction = SLTU;
+			}
+			case 0b100:{
+				obj.Instruction = XOR;
+			}
+		
+			case 0b110:{
+				obj.Instruction = OR;
+			}
+			case 0b111:{
+				obj.Instruction = AND;
+			}
+		}
+	}
+}
 
 
 int main(int argc, char const *argv[])
 {
 	Decode obj;
-	uint32_t instr = 0xdeadbe03;
+	uint32_t instr = 0xdeadbe00;
 	set_type_and_decode(obj, instr);
+
 	return 0;
 }
 
